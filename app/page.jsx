1c8369dc4dee2160705fa442f6cb9a1f9162c53d
Rahul Bote <rahul.bote@gmail.com>
2023-09-24 13:06:28 +0530

    first

---
+++ app/page.jsx
@@ -0,0 +1,21 @@
+import Cards from '@/app/components/Cards'
+
+
+const page = () => {
+  return (
+    <div>
+      <div className='flex justify-center p-4'>
+     <h1 className='bg-white p-2 rounded-xl text-black '>Movies</h1>
+
+      </div>
+
+      <Cards></Cards>
+
+
+
+
+    </div>
+  )
+}
+
+export default page
